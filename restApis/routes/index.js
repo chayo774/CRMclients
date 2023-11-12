247cb0a85b701392f18503d1ce775cf2a1800941
miguelmc <mcatedral24@gmail.com>
2023-11-12 04:37:13 -0600

    The start of the app couldn't be created

---
+++ restApis/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router();
+const clienteController = require('../controllers/clienteController')
+
+module.exports = function(){
+    //Agrega nuevos clientes via POST
+    router.post('/clientes', clienteController.nuevoCliente);
+    return router;
+}
